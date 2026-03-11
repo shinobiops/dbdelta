be0f04d7bdecfa1b278f9f987b0e5fcd69046568
Mircea Danila Dumitrescu <contact@coredump.consulting>
2026-03-11 19:24:44 +0000

    feat: register all 28 introspectors in orchestrator

--- src/introspector/index.js
+++ src/introspector/index.js
@@ -2,10 +2,62 @@ import { connect } from '../connection.js';
 import { identityKey } from './identity.js';
 import { introspectSchemas } from './schemas.js';
 import { introspectExtensions } from './extensions.js';
+import { introspectTypes } from './types.js';
+import { introspectTables } from './tables.js';
+import { introspectColumns } from './columns.js';
+import { introspectConstraints } from './constraints.js';
+import { introspectIndexes } from './indexes.js';
+import { introspectSequences } from './sequences.js';
+import { introspectFunctions } from './functions.js';
+import { introspectViews } from './views.js';
+import { introspectMaterializedViews } from './materialized-views.js';
+import { introspectTriggers } from './triggers.js';
+import { introspectRules } from './rules.js';
+import { introspectPolicies } from './policies.js';
+import { introspectOperators } from './operators.js';
+import { introspectOpClasses } from './opclasses.js';
+import { introspectAggregates } from './aggregates.js';
+import { introspectCasts } from './casts.js';
+import { introspectCollations } from './collations.js';
+import { introspectTextSearch } from './text-search.js';
+import { introspectStatistics } from './statistics.js';
+import { introspectFdw } from './fdw.js';
+import { introspectUserMappings } from './user-mappings.js';
+import { introspectPublications } from './publications.js';
+import { introspectSubscriptions } from './subscriptions.js';
+import { introspectEventTriggers } from './event-triggers.js';
+import { introspectRoles } from './roles.js';
+import { introspectGrants } from './grants.js';
 
 const introspectors = [
   introspectSchemas,
   introspectExtensions,
+  introspectTypes,
+  introspectTables,
+  introspectColumns,
+  introspectConstraints,
+  introspectIndexes,
+  introspectSequences,
+  introspectFunctions,
+  introspectViews,
+  introspectMaterializedViews,
+  introspectTriggers,
+  introspectRules,
+  introspectPolicies,
+  introspectOperators,
+  introspectOpClasses,
+  introspectAggregates,
+  introspectCasts,
+  introspectCollations,
+  introspectTextSearch,
+  introspectStatistics,
+  introspectFdw,
+  introspectUserMappings,
+  introspectPublications,
+  introspectSubscriptions,
+  introspectEventTriggers,
+  introspectRoles,
+  introspectGrants,
 ];
 
 export async function introspect(fromUrl, toUrl, schemas) {
